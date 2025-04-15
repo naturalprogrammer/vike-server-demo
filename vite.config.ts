61cb75521dece9fc38a9c8e333097c88f7603e2c
Sanjay Patel <skpatel20@gmail.com>
2025-04-15 17:18:24 +0530

    Add `vike-server` and remove `@hono/vite-dev-server`

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import devServer from "@hono/vite-dev-server";
 import { defineConfig } from "vite";
 import vike from "vike/plugin";
 
 export default defineConfig({
   plugins: [
     vike(),
-    devServer({
-      entry: "hono-entry.ts",
-
-      exclude: [
-        /^\/@.+$/,
-        /.*\.(ts|tsx|vue)($|\?)/,
-        /.*\.(s?css|less)($|\?)/,
-        /^\/favicon\.ico$/,
-        /.*\.(svg|png)($|\?)/,
-        /^\/(public|assets|static)\/.+/,
-        /^\/node_modules\/.*/,
-      ],
-
-      injectClientScript: false,
-    }),
     react({}),
     tailwindcss(),
   ],
